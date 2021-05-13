a474d8876b37e8a3afc8321a06d9a9762de60408
Samuel Kraft <samuelkraft@me.com>
2021-05-13 21:48:20 +0100

    Add notion blog

---
+++ pages/[id].js
@@ -0,0 +1,90 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { getDatabase, getPage, getBlocks } from "../lib/notion";
+import Link from "next/link";
+import styles from "./post.module.css";
+
+export default function Post({ page, blocks }) {
+  const name = page.properties.Name.title[0]?.plain_text;
+  return (
+    <div>
+      <Head>
+        <title>{name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <article className={styles.container}>
+        <h1 className={styles.name}>
+          {page.properties.Name.title[0]?.plain_text}
+        </h1>
+        <section>
+          {blocks.map((block) => {
+            const { type, id } = block;
+            const value = block[type];
+            console.log("type", type);
+
+            if (type === "paragraph") {
+              return (
+                <p key={id}>
+                  {value.text.map((p) => {
+                    return p.text.link ? (
+                      <a href={p.text.link.url}>{p.text.content}</a>
+                    ) : (
+                      p.text.content
+                    );
+                  })}
+                </p>
+              );
+            }
+            if (type === "heading_1") {
+              return <h1 key={id}>{value.text.map((p) => p.plain_text)}</h1>;
+            }
+            if (type === "heading_2") {
+              return <h2 key={id}>{value.text.map((p) => p.plain_text)}</h2>;
+            }
+            if (
+              type === "numbered_list_item" ||
+              type === "bulleted_list_item"
+            ) {
+              return (
+                <Fragment key={id}>
+                  {value.text.map((p) => (
+                    <li>{p.plain_text}</li>
+                  ))}
+                </Fragment>
+              );
+            }
+            return (
+              <Fragment key={id}>{`❌ Unsupported type (${type})`}</Fragment>
+            );
+          })}
+          <Link href="/">
+            <a className={styles.back}>← Go home</a>
+          </Link>
+        </section>
+      </article>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const database = await getDatabase("5b53abc87b284beab0c169c9fb695b4d");
+  return {
+    paths: database.map((page) => ({ params: { id: page.id } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { id } = context.params;
+  const page = await getPage(id);
+  const blocks = await getBlocks(id);
+
+  return {
+    props: {
+      page,
+      blocks,
+    },
+    revalidate: 1,
+  };
+};
