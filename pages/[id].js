bec461834415fb307b990372f1072aeefb77a0d5
Samuel Kraft <samuelkraft@me.com>
2021-05-14 13:42:52 +0100

    Improve Text component

--- pages/[id].js
+++ pages/[id].js
@@ -5,25 +5,30 @@ import Link from "next/link";
 import { databaseId } from "./index.js";
 import styles from "./post.module.css";
 
-const Text = ({ value }) => {
-  const {
-    annotations: { bold, code, color, italic, striketrough, underline },
-    text,
-  } = value;
-  return (
-    <span
-      className={[
-        bold ? styles.bold : "",
-        code ? styles.code : "",
-        italic ? styles.italic : "",
-        striketrough ? styles.striketrough : "",
-        underline ? styles.underline : "",
-      ].join(" ")}
-      style={color !== "default" ? { color } : {}}
-    >
-      {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
-    </span>
-  );
+const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((value) => {
+    const {
+      annotations: { bold, code, color, italic, striketrough, underline },
+      text,
+    } = value;
+    return (
+      <span
+        className={[
+          bold ? styles.bold : "",
+          code ? styles.code : "",
+          italic ? styles.italic : "",
+          striketrough ? styles.striketrough : "",
+          underline ? styles.underline : "",
+        ].join(" ")}
+        style={color !== "default" ? { color } : {}}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
 };
 
 const renderBlock = (block) => {
@@ -34,70 +39,54 @@ const renderBlock = (block) => {
     case "paragraph":
       return (
         <p>
-          {value.text.map((value) => (
-            <Text value={value} key={value.plain_text} />
-          ))}
+          <Text text={value.text} />
         </p>
       );
       break;
     case "heading_1":
       return (
         <h1>
-          {value.text.map((value) => (
-            <Text value={value} key={value.plain_text} />
-          ))}
+          <Text text={value.text} />
         </h1>
       );
       break;
     case "heading_2":
       return (
         <h2>
-          {value.text.map((value) => (
-            <Text value={value} key={value.plain_text} />
-          ))}
+          <Text text={value.text} />
         </h2>
       );
       break;
     case "heading_3":
       return (
         <h3>
-          {value.text.map((value) => (
-            <Text value={value} key={value.plain_text} />
-          ))}
+          <Text text={value.text} />
         </h3>
       );
       break;
     case "bulleted_list_item":
     case "numbered_list_item":
       return (
-        <>
-          {value.text.map((value) => (
-            <li key={value.plain_text}>
-              <Text value={value} />
-            </li>
-          ))}
-        </>
+        <li>
+          <Text text={value.text} />
+        </li>
       );
       break;
     case "to_do":
       return (
-        <div key={id}>
+        <div>
           <label htmlFor={id}>
             <input type="checkbox" id={id} defaultChecked={value.checked} />{" "}
-            {value.text.map((value) => (
-              <Text value={value} key={value.plain_text} />
-            ))}
+            <Text text={value.text} />
           </label>
         </div>
       );
       break;
     case "toggle":
       return (
-        <details key={id}>
+        <details>
           <summary>
-            {value.text.map((value) => (
-              <Text value={value} key={value.plain_text} />
-            ))}
+            <Text text={value.text} />
           </summary>
           It's a toggle!
         </details>
