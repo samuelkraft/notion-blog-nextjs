0e9fc8c5e7fb15c7c57e64a4044e2f2ee58672fb
sriram-vasudevan <51313737+sriram-vasudevan@users.noreply.github.com>
2021-05-15 00:48:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
