72c71566d197a19081d3a958a571c8d5c390e45c
Samuel Kraft <samuelkraft@me.com>
2021-08-16 10:47:49 +0200

    Upgrade packages

--- pages/index.js
+++ pages/index.js
@@ -87,6 +87,15 @@ export default function Home({ posts }) {
               </li>
             );
           })}
+          <ol className={styles.posts}>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <Link href={`/${post.id}`}>
+                  <Text text={post.properties.Name.title} />
+                </Link>
+              </li>
+            ))}
+          </ol>
         </ol>
       </main>
     </div>
