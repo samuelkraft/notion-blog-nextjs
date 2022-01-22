acacfe4b766ae776cfca2e1133b775120b7569d9
Samuel Kraft <samuelkraft@me.com>
2022-01-22 23:25:11 +0100

    Fix crash for images without captions

--- pages/[id].js
+++ pages/[id].js
@@ -92,7 +92,7 @@ const renderBlock = (block) => {
     case "image":
       const src =
         value.type === "external" ? value.external.url : value.file.url;
-      const caption = value.caption ? value.caption[0].plain_text : "";
+      const caption = value.caption ? value.caption[0]?.plain_text : "";
       return (
         <figure>
           <img src={src} alt={caption} />
