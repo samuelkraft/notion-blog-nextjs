3ed15cce1d9e73902fee12eba71a16c752db3b66
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-03-29 22:12:52 -0400

    calendly-update

--- pages/contact.js
+++ pages/contact.js
@@ -33,7 +33,7 @@ function Contact() {
 
         
 <div>
-    <div class="calendly-inline-widget" data-url="https://calendly.com/ayush110/30min" style={{ minWidth: '320px', height:"70px" }}></div>
+    <div class="calendly-inline-widget" data-url="https://calendly.com/ayush110/30min" style={{ minWidth: '320px', height:"700px" }}></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
 </div>
    
