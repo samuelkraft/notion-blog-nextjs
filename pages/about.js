d5979b7f667cc391bafb0ef10d5258b7f04ba826
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-04-29 13:57:03 -0400

    bug-fix

--- pages/about.js
+++ pages/about.js
@@ -1,34 +1,34 @@
 import Head from "next/head";
-import Link from "next/link";
+
 import Footer from "../components/Footer.js";
-import { Text } from "./[id].js";
+
 import styles from "./index.module.css";
 import Navbar from "../components/Navbar.js"
 
-function About({ blogPosts }) {
+function About() {
     
   return (
-    <div class="flex flex-col min-h-screen ">
+    <div className="flex flex-col min-h-screen ">
       <Head>
         <title>Ayush's Blog</title>
         <link rel="icon" href="/favicon-modified.png" />
       </Head>
 
-    <div class="flex-grow">
+    <div className="flex-grow">
       <Navbar/>
       
       <main className={styles.container}>
-      <div class="w-4/5 mx-auto mt-32 ">
+      <div className="w-4/5 mx-auto mt-32 ">
             
 
             <div >
-            <h2 class="text-2xl font-bold text-left mb-6">About</h2>
-            <h2 class="text-md text-left mb-3">Hey, this page is still being worked on! In the meantime, feel free to reach out :) </h2>
+            <h2 className="text-2xl font-bold text-left mb-6">About</h2>
+            <h2 className="text-md text-left mb-3">Hey, this page is still being worked on! In the meantime, feel free to reach out :) </h2>
             </div>
             
 
-            <div class="mt-6" >
-                    <a href="contact" class="inline-block text-m px-4 py-2 my-auto leading-none border rounded-full text-white bg-black border-black hover:border-stone-400 hover:text-stone-400 hover:bg-white mt-4 sm:mt-0">contact me</a>
+            <div className="mt-6" >
+                    <a href="contact" className="inline-block text-m px-4 py-2 my-auto leading-none border rounded-full text-white bg-black border-black hover:border-stone-400 hover:text-stone-400 hover:bg-white mt-4 sm:mt-0">contact me</a>
                 </div>
 
                 </div>
