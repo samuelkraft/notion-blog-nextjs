58286758d31eb21c4bdd667344bb5dd9c70f2a34
layfredrc <47161258+layfredrc@users.noreply.github.com>
2022-12-31 16:31:52 +0100

    Legal Services Page Content Integration

--- pages/[locale]/services/administrative-services/index.jsx
+++ pages/[locale]/services/administrative-services/index.jsx
@@ -158,7 +158,7 @@ const AdministritiveServices = ({}) => {
 							</Heading>
 							<Heading>{t("section1_text2")}</Heading>
 						</TextContent>
-						<ImageWrapperFullWidth src='https://images.unsplash.com/photo-1518005020951-eccb494ad742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80'></ImageWrapperFullWidth>
+						<ImageWrapperFullWidth src='https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'></ImageWrapperFullWidth>
 					</LayoutColumn>
 				</div>
 			</HeroContainer>
