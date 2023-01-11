a7d7c7f191d34fe9aa6f191c8833087029c13237
layfredrc <47161258+layfredrc@users.noreply.github.com>
2023-01-12 00:34:59 +0100

    [FIX] smoother animation on HeroAboutPage

--- components/about/HeroAboutPage.jsx
+++ components/about/HeroAboutPage.jsx
@@ -11,7 +11,7 @@ const HeroAboutPage = () => {
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{
-						delay: 1.5,
+						delay: 2,
 						default: {
 							duration: 1,
 							ease: [0, 0.71, 0.2, 1.01],
@@ -28,7 +28,7 @@ const HeroAboutPage = () => {
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{
-						delay: 1.5,
+						delay: 2.5,
 						default: {
 							duration: 1,
 							ease: [0, 0.71, 0.2, 1.01],
@@ -45,7 +45,7 @@ const HeroAboutPage = () => {
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{
-						delay: 1.5,
+						delay: 2.25,
 						default: {
 							duration: 1,
 							ease: [0, 0.71, 0.2, 1.01],
